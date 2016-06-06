216205a2f1a668f9beab794605b750ba4cdf66a1
Checkkung <nerogamertv@gmail.com>
2016-06-06 16:45:07 +0700

    Design page Sign Up

--- app/src/main/java/inseenanpbru/tanongsak/itpbru/MainActivity.java
+++ app/src/main/java/inseenanpbru/tanongsak/itpbru/MainActivity.java
@@ -1,7 +1,9 @@
 package inseenanpbru.tanongsak.itpbru;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+    }   //Main Method
+
+    public void clickSignUpMain(View view) { //กด shif + ctrl + enter
+        startActivity(new Intent(MainActivity.this, SignUpActivity.class) );
+    }   //พยายามพิมพ์ ตัวอักษรตัวแรกแล้วตามด้วยเมนูที่เลื่อนลงมาแทนการพิมพ์
+
+
+
+}   //Main Class นี้คือคลาสหลัก
